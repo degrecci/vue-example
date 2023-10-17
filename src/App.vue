a43849b7c9db28b973b247bf5fc15d35e1e260d3
Henrique Degrecci <hdegrecci@gmail.com>
2023-10-17 20:22:13 -0300

    Use vue ref

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,25 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const count = ref(0)
+
+function increment() {
+  count.value++
+}
+</script>
 
 <template>
   <header>
     <div class="wrapper"></div>
   </header>
   <div id="app">
-    <button @click="count++">{{ count }}</button>
+    <div class="p-10">
+      <button
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        @click="increment"
+      >
+        Count is: {{ count }}
+      </button>
+    </div>
   </div>
 </template>
