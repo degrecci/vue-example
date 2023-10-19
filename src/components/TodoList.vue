260dd3f162ecfa7c110c1849ee0ffec0db52010b
Henrique Degrecci <hdegrecci@gmail.com>
2023-10-19 20:30:09 -0300

    Stylize todo list

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -23,13 +23,21 @@ function removeTodo(todo) {
   <form @submit.prevent="addTodo">
     <input
       v-model="newTodo"
-      class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5"
+      class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 mr-2"
     />
-    <button>Add Todo</button>
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      Add Todo
+    </button>
   </form>
-  <ul>
-    <li v-for="todo in todos" :key="todo.id">
-      {{ todo.text }}<button @click="removeTodo(todo)">X</button>
+  <ul class="w-1/4">
+    <li v-for="todo in todos" :key="todo.id" class="p-1 border rounded-lg flex justify-between">
+      {{ todo.text
+      }}<button
+        @click="removeTodo(todo)"
+        class="ml-2 bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-3 rounded"
+      >
+        X
+      </button>
     </li>
   </ul>
 </template>
