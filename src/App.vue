5800095d4b922b5e39ee4ab391656668c57b3dd0
Henrique Degrecci <hdegrecci@gmail.com>
2023-10-18 22:59:35 -0300

    if/else on vue

--- src/App.vue
+++ src/App.vue
@@ -12,14 +12,20 @@ const text = ref('')
 function onInput(e: Event) {
   text.value = (e?.target as HTMLInputElement).value
 }
+
+const onOff = ref(true)
+
+function toggle() {
+  onOff.value = !onOff.value
+}
 </script>
 
 <template>
   <header>
     <div class="wrapper"></div>
   </header>
-  <div id="app">
-    <div class="p-10">
+  <div id="app" class="p-6">
+    <div class="p-10 border rounded-sm mb-2">
       <button
         class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
         @click="increment"
@@ -27,7 +33,7 @@ function onInput(e: Event) {
         Count is: {{ count }}
       </button>
     </div>
-    <div class="p-10">
+    <div class="p-10 border rounded-sm mb-2">
       <input
         :value="text"
         @input="onInput"
@@ -35,6 +41,12 @@ function onInput(e: Event) {
         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5"
       />
       <p>{{ text }}</p>
+    </div>
+
+    <div class="p-10 border rounded-sm mb-2">
+      <button @click="toggle" class="text-lg">Toggle</button>
+      <h1 v-if="onOff" class="text-green-500 text-base font-bold">ON</h1>
+      <h1 v-else class="text-red-500 text-base font-bold">OFF</h1>
     </div>
   </div>
 </template>
