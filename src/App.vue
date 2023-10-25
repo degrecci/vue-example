0b42754694386467d708c32cc9b487bbee1776a0
Henrique Degrecci <hdegrecci@gmail.com>
2023-10-25 20:59:34 -0300

    Move sidebar to component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-
-const liClasses = 'text-2xl text-white my-2 font-bold'
+import Sidebar from '@/components/Sidebar.vue'
 </script>
 
 <template>
@@ -9,37 +8,7 @@ const liClasses = 'text-2xl text-white my-2 font-bold'
     <div class="wrapper"></div>
   </header>
   <layout id="app">
-    <aside class="sidebar p-6 bg-blue-500">
-      <ul class="w-full">
-        <li :class="liClasses">
-          <RouterLink to="/">home</RouterLink>
-        </li>
-        <li :class="liClasses">
-          <RouterLink to="/text-input">text-input</RouterLink>
-        </li>
-        <li :class="liClasses">
-          <RouterLink to="/count">count</RouterLink>
-        </li>
-        <li :class="liClasses">
-          <RouterLink to="/todo-list">todo-list</RouterLink>
-        </li>
-        <li :class="liClasses">
-          <RouterLink to="/on-mounted">on-mounted</RouterLink>
-        </li>
-        <li :class="liClasses">
-          <RouterLink to="/fetch">fetch</RouterLink>
-        </li>
-        <li :class="liClasses">
-          <RouterLink to="/define-props">define-props</RouterLink>
-        </li>
-        <li :class="liClasses">
-          <RouterLink to="/slots">slots</RouterLink>
-        </li>
-        <li :class="liClasses">
-          <RouterLink to="/toggle">toggle</RouterLink>
-        </li>
-      </ul>
-    </aside>
+    <Sidebar />
     <main class="main p-6">
       <RouterView />
     </main>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+const liClasses = 'text-2xl text-white my-2 font-bold'
+</script>
+<template>
+  <aside class="sidebar p-6 bg-blue-500">
+    <ul class="w-full">
+      <li :class="liClasses">
+        <RouterLink to="/">home</RouterLink>
+      </li>
+      <li :class="liClasses">
+        <RouterLink to="/text-input">text-input</RouterLink>
+      </li>
+      <li :class="liClasses">
+        <RouterLink to="/count">count</RouterLink>
+      </li>
+      <li :class="liClasses">
+        <RouterLink to="/todo-list">todo-list</RouterLink>
+      </li>
+      <li :class="liClasses">
+        <RouterLink to="/on-mounted">on-mounted</RouterLink>
+      </li>
+      <li :class="liClasses">
+        <RouterLink to="/fetch">fetch</RouterLink>
+      </li>
+      <li :class="liClasses">
+        <RouterLink to="/define-props">define-props</RouterLink>
+      </li>
+      <li :class="liClasses">
+        <RouterLink to="/slots">slots</RouterLink>
+      </li>
+      <li :class="liClasses">
+        <RouterLink to="/toggle">toggle</RouterLink>
+      </li>
+    </ul>
+  </aside>
+</template>
