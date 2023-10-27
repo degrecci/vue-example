a37277fffbc8bfe4fedc5683e2530d8ccf74923c
Henrique Degrecci <hdegrecci@gmail.com>
2023-10-27 18:54:41 -0300

    Create form to test

--- src/views/FormView.vue
+++ src/views/FormView.vue
@@ -1,3 +1,23 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import VueButton from '@/components/VueButton.vue'
+import VueInput from '@/components/VueInput.vue'
+
+const name = ref('')
+const lastname = ref('')
+
+function handleSubmit(event: SubmitEvent) {
+  event.preventDefault()
+}
+</script>
 <template>
-  <form>Form</form>
+  <form @onsubmit="handleSubmit">
+    <div class="flex flex-col mb-2 w-1/4">
+      <label>Name</label><VueInput placeholder="type a name" v-model="name" />
+    </div>
+    <div class="flex flex-col mb-4 w-1/4">
+      <label>Lastname</label><VueInput placeholder="type a lastname" v-model="lastname" />
+    </div>
+    <VueButton type="submit">Submit</VueButton>
+  </form>
 </template>
