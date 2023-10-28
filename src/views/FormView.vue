a9c8a62ee9f448397565336f2f66fd0e23340f27
Henrique Degrecci <hdegrecci@gmail.com>
2023-10-27 21:24:26 -0300

    Add radio

--- src/views/FormView.vue
+++ src/views/FormView.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
-const form = ref({ name: '', lastname: '' })
+const form = ref({ name: '', lastname: '', pick: '' })
 </script>
 <template>
   <div class="inline-flex w-full">
@@ -12,6 +12,13 @@ const form = ref({ name: '', lastname: '' })
       <div class="flex flex-col mb-2">
         <label>Lastname</label><input placeholder="type a lastname" v-model="form.lastname" />
       </div>
+      <p>Pick:</p>
+      <input type="radio" id="one" value="One" v-model="form.pick" />
+      <label for="one">One</label>
+      <input type="radio" id="two" value="Two" v-model="form.pick" />
+      <label for="Two">Two</label>
+      <input type="radio" id="three" value="Three" v-model="form.pick" />
+      <label for="Three">Three</label>
     </div>
     <div class="w-2/6 p-4">
       <pre>{{ JSON.stringify(form) }}</pre>
