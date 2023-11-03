a600fab508a4210eb534c608d5845be72d89edb9
Henrique Degrecci <hdegrecci@gmail.com>
2023-11-03 15:34:49 -0300

    Add scenario to add item to todo list

--- src/views/__tests__/TodoListView.test.ts
+++ src/views/__tests__/TodoListView.test.ts
@@ -1,13 +1,25 @@
 import { describe, expect, it } from 'vitest'
-import { render, screen } from '@testing-library/vue'
+import { fireEvent, render, screen } from '@testing-library/vue'
 
 import TodoListViewVue from '../TodoListView.vue'
 
 describe('TodoListView', () => {
-  it('should render', () => {
+  it('should render with vue and react', () => {
     render(TodoListViewVue)
 
     screen.getByText('Vue')
     screen.getByText('React')
+  })
+
+  it('should add new items to list', async () => {
+    render(TodoListViewVue)
+
+    await fireEvent.update(screen.getByPlaceholderText(/type todo/i), 'New item')
+
+    await fireEvent.click(screen.getByText(/add todo/i))
+
+    screen.getByText(/vue/i)
+    screen.getByText(/react/i)
+    screen.getByText(/new item/i)
   })
 })
